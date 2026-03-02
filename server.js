e2453f76b3e695166db534b2faf9733aaddfbc6f
As12iimm <azimdilshadpp@gmail.com>
2026-03-02 22:49:54 +0530

    Fix non-clickable auth buttons by allowing inline handlers via CSP

--- server.js
+++ server.js
@@ -37,7 +37,17 @@ const MessageTemplate = require('./src/models/MessageTemplate');
 const MessageLog = require('./src/models/MessageLog');
 
 const app = express();
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: {
+    useDefaults: true,
+    directives: {
+      "script-src": ["'self'", "'unsafe-inline'"],
+      "style-src": ["'self'", "'unsafe-inline'"],
+      "img-src": ["'self'", "data:", "blob:"],
+      "connect-src": ["'self'"]
+    }
+  }
+}));
 app.use(cors());
 app.use(express.json({ limit: '15mb' }));
 app.use(morgan('dev'));
